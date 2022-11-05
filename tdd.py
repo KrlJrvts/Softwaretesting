1c3c115ab45f1d18cfc6dbd41b31c14b21836edb
Kaarel Järveots <115338028+KrlJrvts@users.noreply.github.com>
2022-11-05 09:58:36 +0200

    Initial release

---
+++ tdd.py
@@ -0,0 +1,6 @@
+"""
+1. You need to create a folder that starts with the name 'test'
+2. You need to create file __init__.py in test folder
+3. You need to create a python script that also starts with 'test'
+4. You need to create python function that starts with 'test'
+"""
