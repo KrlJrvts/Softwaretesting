5641406079485117a4eb4b76cacb614429d449c5
Kaarel Järveots <115338028+KrlJrvts@users.noreply.github.com>
2022-11-05 10:10:43 +0200

    Initial release

---
+++ test/test_additions.py
@@ -0,0 +1,9 @@
+from src.calculator import add
+
+
+def test_when_five_is_added_to_six_then_eleven_is_returned():
+    assert 5 + 6 == 11
+
+
+def test_when_twenty_is_added_to_fifty_then_seventy_is_returned():
+    assert 20 + 50 == 70
