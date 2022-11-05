4cccd62cff7088ba70addbfe872e73fddf9377d6
Kaarel Järveots <115338028+KrlJrvts@users.noreply.github.com>
2022-11-05 14:03:08 +0200

    0.09a build

--- test/test_substractions.py
+++ test/test_substractions.py
@@ -3,9 +3,4 @@ from src.calculator import Calculator
 
 def test_when_five_is_subtracted_from_six_then_one_is_returned():
     calc_sub = Calculator()
-    assert calc_sub.sub(6, 5) == 1
-
-
-def test_operator():
-    calc_sub_2 = Calculator()
-    calc_sub_2.div("Chicken", "egg")
+    assert calc_sub.sub(12467, 7) == 12460
