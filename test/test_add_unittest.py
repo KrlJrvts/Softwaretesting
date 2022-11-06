f1e69150b2b87ef502c8613f16a496ec1ec98a98
Kaarel Järveots <115338028+KrlJrvts@users.noreply.github.com>
2022-11-06 10:55:27 +0200

    0.16a build added unitest

--- test/test_add_unittest.py
+++ test/test_add_unittest.py
@@ -5,4 +5,11 @@ from src.calculator import Calculator
 class RandomTestExamples(unittest.TestCase):
     def test_when_six_is_added_to_five_then_eleven_is_returned(self):
         # assert Calculator().add(6, 5) == 11
-        self.assert_(Calculator().add(6, 5), 11)
+        expected_error = TypeError
+        self.assertRaises(expected_error, Calculator.add, "six", "five")
+
+    def test_other_assertions(self):
+        self.assertEqual(Calculator().add(10, 10), 20)
+
+    def test_assertIN(self):
+        self.assertIn()
